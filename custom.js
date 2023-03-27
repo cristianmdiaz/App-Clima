ff4b7b1394a5955be440b44a254317783c6bb74b
Cristian Diaz <cristianmigueldiaz@gmail.com>
2023-03-27 00:50:42 -0300

    Funcion para llamar a la API y mostrar el clima

--- custom.js
+++ custom.js
@@ -8,12 +8,77 @@ form.addEventListener('submit', (e) => {
 
   if(nameCity.value === '' || nameCountry.value === ''){
     showError('Ambos campos son obligatorios...');
+    return;
   }
 
+  callAPI(nameCity.value, nameCountry.value);
   // console.log(nameCity.value);
   // console.log(nameCountry.value);
 })
 
+function callAPI(city, country){
+   const apiId = '50b8a938a898deee2dea0c2ce9eb27bc';
+   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiId}`;
+
+   fetch(url)
+      .then(data => {
+        return data.json();
+      })
+      .then(dataJSON =>{
+        if (dataJSON.cod === '404'){
+          showError('Ciudad no encontrada...');
+        } else {
+          clearHTML();
+          showWeather(dataJSON);
+        }
+        // console.log(dataJSON);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+}
+
+function showWeather(data){
+  const {name, main:{temp, temp_min, temp_max}, weather:[arr]} = data;
+
+  const degrees = kelvinToCentigrade(temp);
+  const min = kelvinToCentigrade(temp_min);
+  const max = kelvinToCentigrade(temp_max);
+
+  const content = document.createElement('div');
+  content.innerHTML = `
+  <h5>Clima en ${name}</h5>
+  <img src="https://openweathermap.org/img/wn/${arr.icon}@2x.png" alt="icon">
+  <h2>${degrees}°C</h2>
+  <p>Max: ${max}°C</p>
+  <p>Min: ${min}°C</p>
+  `;
+
+  result.appendChild(content);
+
+  // console.log(name);
+  // console.log(temp);
+  // console.log(temp_min);
+  // console.log(temp_max);
+  // console.log(arr.icon);
+}
+
 function showError(message){
   console.log(message);
+  const alert = document.createElement('p');
+  alert.classList.add('alert-message');
+  alert.innerHTML = message;
+
+  form.appendChild(alert);
+  setTimeout(()=> {
+    alert.remove();
+  }, 3000);
+}
+
+function kelvinToCentigrade(temp){
+  return parseInt(temp - 273.15);
+}
+
+function clearHTML(){
+  result.innerHTML = ''; 
 }
